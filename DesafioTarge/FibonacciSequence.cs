fca74d8130d6e2597bf59e920460a0b7050ee214
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-03-03 00:30:48 -0300

    Initial Commit

---
+++ DesafioTarge/FibonacciSequence.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTarget
+{
+    public class FibonacciSequence
+    {
+        public void GetFibonacciSequence()
+        {
+            Console.WriteLine("Informe um numero:");
+            var target = int.Parse(Console.ReadLine());
+
+            if (!InputValidation(target))
+            {
+                Console.WriteLine("O numero não pode ser negativo");
+            }
+
+            var res = CalculateFibonacciSequence(target);
+            bool IsTargetInRes = res.Contains(target);
+
+            if (!IsTargetInRes)
+            {
+                Console.WriteLine($"{target} não pertence a sequencia de fibonacci");
+            }else
+            {
+                Console.WriteLine($"{target} pertence a sequencia de fonanacci");
+                Console.WriteLine($"Sequência de Fibonacci até {target}: {string.Join(", ", res)}");
+            }
+
+        }
+
+        public List<int> CalculateFibonacciSequence(int target)
+        {
+
+            var actualNumber = 1;
+            List<int> res = new List<int> { 0, 1 };
+
+            while (res[res.Count() - 1] < target)
+            {
+                int nextNumber = res[^1] + res[^2];
+                res.Add(nextNumber);
+            }
+
+            return res;
+        }
+
+        private bool InputValidation(int input)
+        {
+            return input >= 0;
+        }
+    }
+}
---
+++ DesafioTarget/DesafioTarget.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTarget
+{
+    public class DesafioTarget
+    {
+
+        public List<int> FibonacciSequence(int target)
+        {
+            if (target < 0)
+            {
+                Console.WriteLine("This number does not belong to fibonacci sequence");
+                return new List<int>();
+            }
+
+
+            var actualNumber = 0;
+            List<int> res = new List<int> { 0, 1 };
+
+            while(actualNumber <= target)
+            {
+                var actualIndex = res.Count();
+                actualNumber += res[actualIndex - 1];
+                res.Add(actualNumber);
+            }
+
+            if(res.Last() == target)
+            {
+                Console.WriteLine("This number belongs to fibonacci sequence");
+                return res;
+            }
+
+            Console.WriteLine("This number does not belong to fibonacci sequence");
+            return new List<int>();
+        }
+    }
+}
