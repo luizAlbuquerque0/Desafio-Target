165d2d3b3943fc7db926a98093695d933f4034a9
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-03-03 00:43:21 -0300

    Initial Commit

---
+++ DesafioTarge/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioTarget;
+
+//instaciando a classe FibonacciSequence
+var fibonacci = new FibonacciSequence();
+
+//Chamando seu método GetFibonacciSequence que abirá o console pedindo um input,
+//O método verificará se o número inserido pertence à sequência de Fibonacci e, se sim, retornará a sequência até esse número.;
+fibonacci.GetFibonacciSequence();
+
+
+//Instanciando a classe StringInverter
+var stringa = new StringInverter();
+
+//Chamando seu método GetStringInderted que abirá o console. Ao digitar uma palavra, o método retornará a palavra invertida.
+stringa.GetStringInderted();
---
+++ DesafioTarge/StringInverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTarget
+{
+    public class StringInverter
+    {
+        public void GetStringInderted()
+        {
+            Console.WriteLine("Escreva a palava que você quer inventer:");
+            var input = Console.ReadLine();
+
+            string invertedString = "";
+
+           for(var i = input.Length - 1; i >= 0; i--)
+            {
+                invertedString += input[i];
+            }
+
+            Console.WriteLine($"Assim fica a palavra invertida {invertedString}");
+        }
+    }
+}
